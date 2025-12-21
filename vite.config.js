b7e94d99ad08bc6ad7d3666f25daa0d547afb4a1
Cassian <dkkim.esq@Gmail.com>
2025-12-21 16:10:54 +0900

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/glasl-escalation/', //https://fluffy-adventure-zrl916l.pages.github.io/
+  base: '/', //https://fluffy-adventure-zrl916l.pages.github.io/
 })
